26b003a5320ec1d0662f49bb2f2370b313a593c8
clockcode <cagdasmuldur@gmail.com>
2025-08-27 00:43:45 -0400

    eslint fixes and hero section improvements

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import Link from "next/link";
-import Image from "next/image";
+// import Image from "next/image";
 import { getDictionary } from "./lib/get-dictionary";
 
-import { CodeCopy } from "./components/code-copy";
 import { Comments } from "./components/comments";
 import { FeaturesGrid } from "./components/features-grid";
 import { RightsideMarketing } from "./components/rightside-marketing";
@@ -13,8 +12,10 @@ import { Button } from "./components/button";
 import { ColourfulText } from "./components/colorful-text";
 import * as Icons from "./components/icons";
 
+// import { WavyBackground } from "./components/wavy-background";
+
 import type { Locale } from "./config/i18n-config";
-import {VideoScroll} from "./components/video-scroll";
+import { VideoScroll } from "./components/video-scroll";
 
 const people = [
   {
@@ -74,47 +75,61 @@ export default async function IndexPage({
             <BackgroundLines className="h-full">
               <div className="flex flex-col pt-4 md:pt-36 lg:pt-36 xl:pt-36">
                 <div className="mt-20">
-                  <div
-                    className="mb-6 max-w-4xl text-left text-4xl font-semibold dark:text-zinc-100 md:text-5xl xl:text-5xl md:leading-[4rem] xl:leading-[4rem]">
-                    {dict.marketing.title || "Ship your apps to the world easier with "}
-                    <ColourfulText text="Saasfly"/>
+                  <div className="mb-6 max-w-4xl text-left text-4xl font-semibold dark:text-zinc-100 md:text-5xl xl:text-6xl md:leading-[4rem] xl:leading-[4rem]">
+                    {dict.marketing.title ||
+                      "Ship your apps to the world easier with "}
+                    <ColourfulText text=" 24/7" />
                   </div>
                 </div>
 
                 <div className="mt-4">
-                  <span className="text-neutral-500 dark:text-neutral-400 sm:text-lg">
-                    {dict.marketing.sub_title || "Your complete All-in-One solution for building SaaS services."}
+                  <span className="text-neutral-500 dark:text-neutral-400 sm:text-lg md:text-xl">
+                    {dict.marketing.sub_title ||
+                      "Your complete All-in-One solution for building SaaS services."}
                   </span>
                 </div>
 
-                <div
-                  className="mb-4 mt-6 flex w-full flex-col justify-center space-y-4 sm:flex-row sm:justify-start sm:space-x-8 sm:space-y-0 z-10">
-                  <Link href="https://github.com/saasfly/saasfly" target="_blank">
-                    <Button
-                      className="bg-blue-600 hover:bg-blue-500 text-white rounded-full text-lg px-6 h-12 font-medium">
+                <div className="mb-4 mt-6 flex w-full flex-col justify-center space-y-4 sm:flex-row sm:justify-start sm:space-x-8 sm:space-y-0 z-10">
+                  <Link
+                    // TODO: Add waitlist link
+                    href="https://"
+                    target="_blank"
+                  >
+                    <Button className="bg-blue-600 hover:bg-blue-500 text-white rounded-full text-lg px-6 h-12 font-medium">
                       {dict.marketing.get_started}
-                      <Icons.ArrowRight className="h-5 w-5"/>
+                      <Icons.ArrowRight className="h-5 w-5" />
                     </Button>
                   </Link>
 
-                  <CodeCopy/>
+                  <Link
+                    href="https://github.com/saasfly/saasfly"
+                    target="_blank"
+                  >
+                    <Button className="text-blue-400 border border-blue-600 hover:border-blue-500 rounded-full text-lg px-6 h-12 font-medium">
+                      {dict.marketing.view_on_github}
+                    </Button>
+                  </Link>
                 </div>
 
-                <div className="flex xl:flex-row flex-col items-center justify-start mt-4 w-full">
+                <div className="flex sm:flex-row flex-col items-center justify-start mt-4 w-full">
                   <div className="flex">
-                    <AnimatedTooltip items={people}/>
+                    <AnimatedTooltip items={people} />
                   </div>
-                  <div className="flex flex-col items-center justify-start ml-8">
-                    <div className="w-[340px]">
-                      <span className="font-semibold">9 </span>
-                      <span className="text-neutral-500 dark:text-neutral-400">{dict.marketing.contributors.contributors_desc}</span>
+                  <div className="flex flex-col items-center justify-start mt-4 ml-8 xl:mt-0">
+                    <div className="w-[300px]">
+                      {/* <span className="font-semibold">9 </span> */}
+                      <span className="text-neutral-500 dark:text-neutral-400 text-xl">
+                        {dict.marketing.contributors.contributors_desc}
+                      </span>
                     </div>
                     <div className="w-[340px]">
-                      <span
-                        className="text-neutral-500 dark:text-neutral-400">{dict.marketing.contributors.developers_first}</span>
-                      <ColourfulText text="2000"/>
-                      <span
-                        className="text-neutral-500 dark:text-neutral-400">{dict.marketing.contributors.developers_second}</span>
+                      {/* <span className="text-neutral-500 dark:text-neutral-400">
+                        {dict.marketing.contributors.developers_first}
+                      </span>
+                      <ColourfulText text="content creators" /> */}
+                      {/* <span className="text-neutral-500 dark:text-neutral-400">
+                        {dict.marketing.contributors.developers_second}
+                      </span> */}
                     </div>
                   </div>
                 </div>
@@ -130,17 +145,38 @@ export default async function IndexPage({
         </div>
       </section>
 
+      {/* Alternative Hero Section */}
+      {/* <section className="container mx-auto"> */}
+      {/* <div className="grid grid-cols-1 gap-10"> */}
+      {/* <div className="flex flex-col items-start h-full"> */}
+      {/* <WavyBackground className="max-w-4xl mx-auto pb-40"> */}
+      {/* <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center"> */}
+      {/* Connect with fans when they need you the most */}
+      {/* </p> */}
+      {/* <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center"> */}
+      {/* Leverage the power of canvas to create a beautiful hero section */}
+      {/* </p> */}
+      {/* </WavyBackground> */}
+      {/* </div> */}
+
+      {/* <div className="hidden h-full w-full xl:block bg-background"> */}
+      {/* <div className="flex flex-col pt-44"> */}
+      {/* TODO: Put Iphone mockup video here */}
+      {/* </div> */}
+      {/* </div> */}
+      {/* </div> */}
+      {/* </section> */}
+
       <section className="container mt-8 md:mt-[-180px] xl:mt-[-180px] mx-auto">
-        <FeaturesGrid dict={dict.marketing.features_grid}/>
+        <FeaturesGrid dict={dict.marketing.features_grid} />
       </section>
 
       <section className="container mt-8 flex flex-col pt-44 mx-auto">
-        <RightsideMarketing dict={dict.marketing.right_side}/>
+        <RightsideMarketing dict={dict.marketing.right_side} />
       </section>
 
-
       <section className="container pt-8">
-        <VideoScroll dict={dict.marketing.video}/>
+        <VideoScroll dict={dict.marketing.video} />
       </section>
 
       <section className="w-full px-8 pt-10 sm:px-0 sm:pt-24 md:px-0 md:pt-24 xl:px-0 xl:pt-24">
@@ -155,7 +191,7 @@ export default async function IndexPage({
           </div>
 
           <div className="w-full overflow-x-hidden">
-            <Comments/>
+            <Comments />
           </div>
         </div>
       </section>
