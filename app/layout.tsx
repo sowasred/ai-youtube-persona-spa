ffb9b1cf1715e4159d81f78cc97b1b35170901d8
clockcode <cagdasmuldur@gmail.com>
2025-09-02 22:59:55 -0400

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleAnalytics gaId="G-70H4ZQ311P" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
